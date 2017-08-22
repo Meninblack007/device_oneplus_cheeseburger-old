9e7a9f0add6aecf4c69c9f7794a945317e98120c
Luca Stefani <luca020400@lineageos.org>
2017-08-22 22:11:27 +0200

    cheeseburger: Build Camera HAL

--- device.mk
+++ device.mk
@@ -161,7 +161,9 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    Snap
+    Snap \
+    camera.device@3.2-impl \
+    android.hardware.camera.provider@2.4-impl
 
 # Connectivity Engine support (CNE)
 PRODUCT_PACKAGES += \
