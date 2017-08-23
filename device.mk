c1c6e62e8d8ebbd2f999c075af1d30413587a090
Luca Stefani <luca020400@lineageos.org>
2017-08-23 16:40:39 +0200

    cheeseburger: Add seccomp policy
    Change-Id: I96531e076c1bacaab810e2b3fb77deb1df448f6a

--- device.mk
+++ device.mk
@@ -354,6 +354,10 @@ PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl \
     sensors.msm8998
 
+# Seccomp policy
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/seccomp_policy/mediacodec.policy:system/vendor/etc/seccomp_policy/mediacodec.policy
+
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service \
