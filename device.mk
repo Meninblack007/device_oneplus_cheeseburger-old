b91b026b9c8dc27a511b2aacc336381fa53c4a66
codeworkx <daniel.hillenbrand@codeworkx.de>
2017-09-03 20:39:13 +0200

    cheeseburger: ril: add __system_property_wait_any shim
    Change-Id: Id1bc827a08a4c0d8110315f1e7dc33dc458bab3a

--- device.mk
+++ device.mk
@@ -357,6 +357,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/seccomp_policy/mediacodec.policy:system/vendor/etc/seccomp_policy/mediacodec.policy
 
+# Shims
+PRODUCT_PACKAGES += \
+    libshims_system_properties
+
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service \
